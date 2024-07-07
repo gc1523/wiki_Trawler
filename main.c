1794dd951d605fbae1983586550f2df784e109e5
Guy C <gc1523@ic.ac.uk>
2024-07-07 23:05:07 +0100

    Setup

---
+++ main.c
@@ -0,0 +1,13 @@
+// Open start page
+// Grab links and store data
+// repeat until N links trawled
+// Build nice graphic
+// Get Mega Cool Super Rich Internship :sunglasses
+
+
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
